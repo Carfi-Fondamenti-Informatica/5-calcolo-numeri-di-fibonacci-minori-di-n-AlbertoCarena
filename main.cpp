1addb4f330e166641d8dc0bc2cb77b9214343fad
AlbertoCarena <100611068+AlbertoCarena@users.noreply.github.com>
2022-04-06 16:19:06 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
-using namespace std;
 
+using namespace std;
 int main() {
-   return 0;
+    int a=1,b=1,c;
+    int n;
+    cin>>n;
+    cout<<"1"<<endl;
+    cout<<"1"<<endl;
+    while (n>=(a+b)) {
+        c=a+b;
+        cout<<c<<endl;
+        b=a;
+        a=c;
+
+    }
+    
+
+    return 0;
 }
